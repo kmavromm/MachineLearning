e0544146f8f6fcd27e6d8c12cf1ad81652bb53aa
kmavromm <katherine.mavrommati@gmail.com>
2020-05-31 14:44:48 -0700

    ex 2 - logistic regression

---
+++ Stanford_Machine_Learning_Hwk/machine-learning-ex2/ex2/sigmoid.m
@@ -0,0 +1,22 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid function
+%   g = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+g = zeros(size(z));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+
+sz = size(z);
+dim = sz(1) * sz(2);
+for i = [1:dim]
+  g(i) = 1/(1 + exp(-z(i)));
+end
+
+
+
+% =============================================================
+
+end
